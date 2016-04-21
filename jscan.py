4da9d82983c954c5409f23ff8f688f2845d4a7ca
Tyler Jordan <tjordan@juniper.net>
2016-04-21 14:21:48 -0400

    Check for duplicate devices.

--- jscan.py
+++ jscan.py
@@ -53,6 +53,10 @@ Rack Menu
 			
 	def add_device(self):
 		ip = raw_input("Enter an ip: ")							# Change this to "input" when using Python 3
+		for device in devices:
+			if ip is device.ip:
+				print ("Device already exists.")
+				return False
 		dev = Device(ip, user=Menu.username, password=Menu.password)
 		try:
 			dev.open()
